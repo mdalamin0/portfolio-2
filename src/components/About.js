c64d2a36d9788913d6a4082696300a77ef2cc293
md al-amin <muhammadalamin809@gmail.com>
2023-06-27 10:32:13 +0800

    banner section done

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+import image from '../assets/about.png'
 
 const About = () => {
-  return <div className='section' id='about'>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <section className="section" id="about" ref={ref}>
+      <div className="container max-auto">
+        <div className="lg:flex items-center">
+          <div className="w-full lg:w-1/2 "><img className="mix-blend-overlay bg-top" src={image} alt="" /></div>
+          <div className="w-full lg:w-1/2 mt-8 lg:mt-0">
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="h3 mb-2">
+              I'am a MERN Stack Developer with almost 1 year of experience.
+            </h3>
+            <p className="mb-6">
+              As a skilled React JS developer, I have a strong foundation in web
+              development and strong passion for creating user-friendly web
+              applications. And I am studying in Computer technology department
+              in Munshiganj Polytechnic Institute and I am recently finished
+              complete web development with programming hero course. I have
+              successfully completed various notable projects that showcase my
+              expertise in React JS Development. For instance, I developed
+              “Shippo-Football-Academy” this is a full-stack website where I
+              utilized React JS, Tailwind CSS, Flowbite, Firebase, Mongodb,
+              Node.js, Express.js. And I have been working with React last 3
+              months.
+            </p>
+            <div className="flex gap-x-6 lg:gap-x-12">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? 
+                    <CountUp start={0} end={1} duration={3}></CountUp>
+                   : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br /> Experience
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? 
+                    <CountUp start={0} end={50} duration={3}></CountUp>
+                   : null}
+                   +
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Projects <br /> Complete
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section className="section" id="home">
+    <section className="min-h-[85vh] lg:min-h-[78vh] flex items-center" id="home">
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           <div className="lg:w-1/2">
@@ -19,7 +19,7 @@ const Banner = () => {
             viewport={{once: false, amount: 0.7}} 
              className="text-6xl leading-[0.8] mb-3 font-bold">It's AL AMIN</motion.h1>
             <motion.div
-            variants={fadeIn('up', 0.3)}
+            variants={fadeIn('up', 0.4)}
             initial='hidden'
             whileInView={'show'}
             viewport={{once: false, amount: 0.7}} 
@@ -40,26 +40,46 @@ const Banner = () => {
                 repeat={Infinity}
               ></TypeAnimation>
             </motion.div>
-            <p className="max-w-lg mx-auto lg:mx-0">
+            <motion.p 
+            variants={fadeIn('up', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}}
+            className="max-w-lg mx-auto lg:mx-0">
               I am skilled MERN Stack developer. I have a good knowledge in web
               development. I like to work specially on web developing and
               software. I can create awesome user Friendly web application. I
               always try to be honest in my work, I want to go far in the future
               by working with more new technologies.
-            </p>
-            <div className=" flex gap-x-6 max-w-max mx-auto lg:mx-0 my-10 items-center">
+            </motion.p>
+            <motion.div 
+            variants={fadeIn('up', 0.6)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}}
+            className=" flex gap-x-6 max-w-max mx-auto lg:mx-0 mb-8 mt-10 items-center">
               <button className="btn h-[48px] px-2 md:px-6 text-sm mr-2 lg:mr-4 flex items-center">Download Resume <BiSolidDownload className="ml-2 text-xl"></BiSolidDownload> </button>
               <a href="#" className="text-gradient btn-link">My Portfolio</a>
-            </div>
-            <div className="flex text-xl gap-x-6 max-w-max mx-auto  lg:mx-0 mb-10">
+            </motion.div>
+            <motion.div 
+            variants={fadeIn('up', 0.7)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.6}}
+             className="flex text-xl gap-x-6 max-w-max mx-auto  lg:mx-0 mb-10">
               <a href=""><FaGithub className="text-2xl"></FaGithub></a>
               <a href=""><FaLinkedin className="text-2xl"></FaLinkedin></a>
               <a href=""><FaFacebook className="text-2xl"></FaFacebook></a>
-            </div>
+            </motion.div>
           </div>
-          <div className="lg:w-1/2">
+          <motion.div 
+          variants={fadeIn('down', 0.5)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.7}}
+          className="lg:w-1/2">
             <img className="w-[300px] mx-auto md:w-[400px] rounded-full " src={Image} alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
