cadf36decf54d67d87585765b36eb4d83754488e
md al-amin <muhammadalamin809@gmail.com>
2023-06-27 11:24:38 +0800

    aboute me section done

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import CountUp from "react-countup";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
-import image from '../assets/about.png'
+import image from "../assets/about.png";
 
 const About = () => {
   const [ref, inView] = useInView({
@@ -12,9 +12,22 @@ const About = () => {
   return (
     <section className="section" id="about" ref={ref}>
       <div className="container max-auto">
-        <div className="lg:flex items-center">
-          <div className="w-full lg:w-1/2 "><img className="mix-blend-overlay bg-top" src={image} alt="" /></div>
-          <div className="w-full lg:w-1/2 mt-8 lg:mt-0">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <img className="lg:h-[640px]" src={image} alt="" />
+          </motion.div>
+          <motion.div
+          variants={fadeIn("left", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="flex-1">
             <h2 className="h2 text-accent">About me.</h2>
             <h3 className="h3 mb-2">
               I'am a MERN Stack Developer with almost 1 year of experience.
@@ -24,20 +37,12 @@ const About = () => {
               development and strong passion for creating user-friendly web
               applications. And I am studying in Computer technology department
               in Munshiganj Polytechnic Institute and I am recently finished
-              complete web development with programming hero course. I have
-              successfully completed various notable projects that showcase my
-              expertise in React JS Development. For instance, I developed
-              “Shippo-Football-Academy” this is a full-stack website where I
-              utilized React JS, Tailwind CSS, Flowbite, Firebase, Mongodb,
-              Node.js, Express.js. And I have been working with React last 3
-              months.
+              complete web development with programming hero course.
             </p>
             <div className="flex gap-x-6 lg:gap-x-12">
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {inView ? 
-                    <CountUp start={0} end={1} duration={3}></CountUp>
-                   : null}
+                  <CountUp start={0} end={1} duration={3}></CountUp>
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
                   Years of <br /> Experience
@@ -45,17 +50,25 @@ const About = () => {
               </div>
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {inView ? 
-                    <CountUp start={0} end={50} duration={3}></CountUp>
-                   : null}
-                   +
+                  <CountUp start={0} end={50} duration={3}></CountUp>+
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
                   Projects <br /> Complete
                 </div>
               </div>
             </div>
-          </div>
+            <div>
+              <div
+                
+                className=" flex gap-x-6 mb-8 mt-10 items-center"
+              >
+                <button className="btn btn-sm">Contact me</button>
+                <a href="#" className="text-gradient btn-link">
+                  My Portfolio
+                </a>
+              </div>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
